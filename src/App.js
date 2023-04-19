a9e57f8615b46d04759a9c41cd4b85e32d321a30
alain-kubwayo <ahkubwayo@gmail.com>
2023-04-19 16:15:23 +0200

    refactor: move server import to App component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import './services/server';
+
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Wrapper from "./components/layout/Wrapper";
