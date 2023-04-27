5f4c9ab6e5392d3852056a41e2038f0f22d85868
alain-kubwayo <ahkubwayo@gmail.com>
2023-04-27 12:05:51 +0200

    feat: add a list of vans to host vans page

--- src/pages/host/HostVans.jsx
+++ src/pages/host/HostVans.jsx
@@ -1,6 +1,46 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 const HostVans = () => {
+    const [vans, setVans] = useState([]);
+
+    useEffect(() => {
+        const fetchHostVans = async () => {
+            const response = await fetch('/api/host/vans');
+            const data = await response.json();
+            setVans(data.vans);
+        }
+        fetchHostVans();
+    }, [])
+
     return ( 
-        <h1>HostVans page</h1>
+        <div>
+            <h1 className="my-4 text-xl font-bold">Your Listed Vans</h1>
+            <div>
+                {
+                    vans.length > 0 ? (
+                    <div class="grid grid-cols-2 gap-4">
+                        {
+                            vans.map(van => (
+                                <Link 
+                                    to={`/host/vans/${van.id}`}
+                                    key={van.id}
+                                >
+                                    <div>
+                                        <img src={van.imageUrl} alt={van.name} />
+                                        <div>
+                                            <h3>{van.name}</h3>
+                                            <p>{van.price}</p>
+                                        </div>
+                                    </div>
+
+                                </Link>
+                            ))
+                        }
+                    </div>) : <div>This host has no vans listed.</div>
+                }
+            </div>
+        </div>
     );
 }
  
